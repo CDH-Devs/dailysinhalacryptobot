aa016cefe0a5609f752d2659ba04fa9a7174d8b3
C D H <chamod.binance.lk@gmail.com>
2025-11-27 14:52:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const CONFIG = {
     OWNER_CHAT_ID: "6762786795",
     
     // 🛑 ඔබේ අලුත්ම Gemini API Key එක
-    GEMINI_API_KEY: "AIzaSyB2g730Cnuove9jO2CVBizrUcXKo-cIOWs",
+    GEMINI_API_KEY: "AIzaSyDACg18fwe-yTZY8OvPykUhXGTQ13OfEkM",
     
     // Telegram API Endpoint Base URL එක
     TELEGRAM_API_BASE: `https://api.telegram.org/bot5100305269:AAEHxCE1z9jCFZl4b0-yoRfVfojKBRKSL0Q`,
